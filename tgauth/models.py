6cc066c38cccf2ac56c0e37609ae5a87f99a0475
Dartamid <shabalin.dmm@yandex.ru>
2024-08-22 00:36:43 +0500

    get access

--- tgauth/models.py
+++ tgauth/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class BotUser(models.Model):
+    tg_id = models.CharField(max_length=200, primary_key=True)
+    access_token = models.CharField(max_length=300)
--- tgauth/views.py
+++ tgauth/views.py
@@ -1,5 +1,24 @@
+import re
+import json
+import base64
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from requests import get
+
+
+def lfm_access_token(url_params):
+    lfm_request = get(
+        url='https://api2.lowfuelmotorsport.com/auth/steam/handle',
+        params=url_params
+    )
+    match = r'(?<=data=).*?(?=&redirect)'
+    lfm_raw = re.findall(match, lfm_request.url).pop()
+    lfm_token = json.loads(
+        base64.urlsafe_b64decode(lfm_raw + "=" * divmod(len(lfm_raw), 4)[1]).decode('utf-8')
+    )['accessToken']
+
+    return lfm_token
 
 
 def auth(request):
@@ -7,7 +26,13 @@ def auth(request):
 
 
 def login(request):
-    return HttpResponse('hello world')
+    data = request.GET
+    at = lfm_access_token(data)
+    lfm_user = get(
+        url='https://api2.lowfuelmotorsport.com/api/user',
+        headers={'Authorization': 'Bearer ' + at}
+    ).json()
+    return HttpResponse()
 
 
 def success(request):
